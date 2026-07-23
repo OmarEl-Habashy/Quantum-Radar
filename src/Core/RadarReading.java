8e8083cbc4a8373567e22d8895a8fadf54b95319
Omar Emad El-Habashy <O.El-Haba06695@student.aast.edu>
2026-07-23 17:12:46 +0300

    RadarReading added

---
+++ src/Core/RadarReading.java
@@ -0,0 +1,43 @@
+package Core;
+
+
+import models.CarType;
+
+import java.time.LocalDate;
+
+public class RadarReading {
+    private final String plateNumber;
+    private final models.CarType carType;
+    private final java.time.LocalDate date;
+    private final int speed;
+    private final boolean seatbeltStatus;
+
+
+    public RadarReading(String plateNumber, CarType carType, LocalDate date, int speed, boolean seatbeltStatus) {
+        this.plateNumber = plateNumber;
+        this.carType = carType;
+        this.date = date;
+        this.speed = speed;
+        this.seatbeltStatus = seatbeltStatus;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isSeatbeltFastened() {
+        return seatbeltStatus;
+    }
+}
