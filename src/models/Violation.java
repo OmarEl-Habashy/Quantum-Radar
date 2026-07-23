09ceeb7068a24d6fe3d3406078c48299d595a02c
Omar Emad El-Habashy <O.El-Haba06695@student.aast.edu>
2026-07-23 16:14:39 +0300

    Violation added

---
+++ src/models/Violation.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Violation {
+    private final String ruleName;
+    private final String description;
+    private int fee;
+
+
+
+    public Violation(String ruleName, String description, int fee) {
+        this.ruleName = ruleName;
+        this.description = description;
+        this.fee = fee;
+    }
+
+    public String getRuleName(){
+        return ruleName;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public int getFee(){
+        return fee;
+    }
+
+    @Override
+    public String toString(){
+        return "- " + description + " : " + fee + " EGP";
+    }
+}
